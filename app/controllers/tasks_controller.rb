4a498050b62b905b1ffe719731c581e48f11df8b
Petya Georgieva <pettya.gg@gmail.com>
2024-07-02 17:31:08 +0200

    Update and delete function created

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,8 +16,30 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-    redirect_to task_path(@task)
+      redirect_to task_path(@task)
     else render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    if @task.destroy
+      redirect_to tasks_path
+    else
+      redirect_to tasks_path, status: :see_other
     end
   end
 
